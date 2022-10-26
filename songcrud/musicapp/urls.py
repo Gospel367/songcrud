3f77e796888355693389a97e35ee2e2519bb2e03
Gospel367 <Ferdinandchinaza@gmail.com>
2022-10-26 21:32:38 +0100

    Edit the urls.py file for api

--- songcrud/musicapp/urls.py
+++ songcrud/musicapp/urls.py
@@ -6,11 +6,11 @@ urlpatterns = format_suffix_patterns([
     path('api', views.api_root),
     path('', views.Home.as_view(), name= 'homeview'),
     path('api/songs/', views.SongList.as_view(), name= 'song_list'),
-    path('api/song-detail/<int:pk>/', views.SongDetail.as_view(), name= 'song_detail'),
+    path('api/songs/<int:pk>/', views.SongDetail.as_view(), name= 'song_detail'),
     path('api/artistes/', views.ArtisteList.as_view(), name = "artist_list"),
-    path('api/artist-detail/<int:pk>/', views.ArtisteDetail.as_view(), name = "artist_detail"),
+    path('api/artistes/<int:pk>/', views.ArtisteDetail.as_view(), name = "artist_detail"),
     path('api/lyrics/', views.LyricList.as_view(), name ='lyric_list'),
-    path('api/lyric-detail/<int:pk>/', views.LyricDetail.as_view(), name='lyric_detail'),
+    path('api/lyrics/<int:pk>/', views.LyricDetail.as_view(), name='lyric_detail'),
 
 ])
 
